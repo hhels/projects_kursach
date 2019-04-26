4592c867fae60402bc7e37b9422440767f43decc
hhels <hels-hels@list.ru>
2019-04-26 12:52:53 +0300

    Работник может посмотреть кто к нему записан

--- fitnes/MainWorkers.cs
+++ fitnes/MainWorkers.cs
@@ -18,12 +18,25 @@ namespace fitnes
         {
             InitializeComponent();
             _workers = CurrentUser.Login;
+            var hours = new[] { "9-10", "10-11", "11-12", "13-14", "14-15", "15-16" };
+            var hoursSelect = string.Join(",", hours.Select(x => $"T.`{x}`"));
+
+            var id = CurrentUser.UserId;
+            var workerid = DbConnector.ExecuteScalar($"SELECT idWorkers FROM Workers WHERE idUser = {id}");
+            dataGridView1.DataSource = DbConnector.ExecuteQuery($"SELECT * FROM Records WHERE idWorker = {workerid} ");
+            dataGridView1.DataSource = DbConnector.ExecuteQuery($@"SELECT  T.date, W.Position, U.LastName, {hoursSelect} FROM Records AS R
+JOIN Time AS T ON T.idtime = R.time_idtime
+JOIN Workers AS W ON W.idWorkers = R.idWorker
+JOIN Users AS U ON U.idUsers = R.Clients_idUsers
+WHERE W.idWorkers = {workerid}");
         }
 
         private void MainWorkers_Load(object sender, EventArgs e)
         {
             textBox1.Text = _workers;
-            dataGridView1.DataSource = DbConnector.ExecuteQuery($"  SELECT LastName, FirstName FROM Users WHERE idUsers IN  ( SELECT Clients_idUsers FROM Records WHERE idWorker = (SELECT idWorkers FROM Workers WHERE idUser = (SELECT idUsers FROM Users WHERE Login = '{textBox1.Text}')))");
+            //dataGridView1.DataSource = DbConnector.ExecuteQuery($"  SELECT LastName, FirstName FROM Users WHERE idUsers IN  ( SELECT Clients_idUsers FROM Records WHERE idWorker = (SELECT idWorkers FROM Workers WHERE idUser = (SELECT idUsers FROM Users WHERE Login = '{textBox1.Text}')))");
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
